1972de21b14bdb832067629855b02d2d19f7db3c
RichardOhata <richardohata@gmail.com>
2021-11-03 10:08:54 -0700

    Formatted Code (a little bit and authenticated Firebase

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBWgWCrH5ealTgDWsAuheVr0PK3E51YNfo",
+    authDomain: "bby10-comp1800-c3fb5.firebaseapp.com",
+    projectId: "bby10-comp1800-c3fb5",
+    storageBucket: "bby10-comp1800-c3fb5.appspot.com",
+    messagingSenderId: "781675691111",
+    appId: "1:781675691111:web:c52e0d6e8bac1e19dba8c2"
+  };
+  
+  // Initialize Firebase
+  const app = firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
